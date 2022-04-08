c00ae4b10050514c4306ca5c694d8dd15d3bc0eb
cyberc001 <cyb3rc001@yandex.ru>
2022-04-08 22:22:56 +0300

    Moved color bit from an uint8 integer to MSB in node size, in virtual memory allocator

--- modules/vmemory/allocator.c
+++ modules/vmemory/allocator.c
@@ -14,6 +14,11 @@
 #define TREE_DIR_CHILD(n) ((n) == ((n)->parent)->child[TREE_DIR_RIGHT] ? TREE_DIR_RIGHT : TREE_DIR_LEFT)
 #define TREE_GET_SIBLING(n) ((n)->parent ? (n)->parent->child[TREE_DIR_CHILD(n)] : NULL)
 
+#define TREE_GET_CLR(n) ((uint64_t)(n)->size >> 63)
+#define TREE_SET_CLR(n, clr) { if(clr) (n)->size = ((uint64_t)(n)->size | ((uint64_t)1 << 63)); else (n)->size = ((uint64_t)(n)->size & ~((uint64_t)1 << 63)); }
+#define TREE_GET_SIZE(n) ((uint64_t)(n)->size & ~((uint64_t)1 << 63))
+#define TREE_SET_SIZE(n, sz) { (n)->size = (sz) & ~((uint64_t)1 << 63); }
+
 typedef struct node node;
 struct node {
 	void* addr;
@@ -21,7 +26,6 @@ struct node {
 
 	node *child[2];
 	node *parent;
-	uint8_t color;
 };
 
 static struct {
@@ -74,9 +78,9 @@ void allocator_init(uint64_t memory_limit)
 	node_mem_pool_init();
 	alloc_tree.root = alloc_node();
 	alloc_tree.root->addr = (void*)0;
-	alloc_tree.root->size = memory_limit;
+	TREE_SET_SIZE(alloc_tree.root, memory_limit);
 	alloc_tree.root->child[0] = alloc_tree.root->child[1] = alloc_tree.root->parent = NULL;
-	alloc_tree.root->color = TREE_CLR_BLACK;
+	TREE_SET_CLR(alloc_tree.root, TREE_CLR_BLACK);
 }
 
 
@@ -86,7 +90,7 @@ void* allocator_alloc(uint64_t size)
 	if(n == (void*)-1)
 		return n;
 
-	if(n->size == size)
+	if(TREE_GET_SIZE(n) == size)
 	{ // perfect fit
 		void* ret = n->addr;
 		alloc_tree_delete(n);
@@ -95,7 +99,7 @@ void* allocator_alloc(uint64_t size)
 	else
 	{ // n->size > size, shrinking the free space and shifting addr up
 		void* ret = n->addr;
-		n->size -= size;
+		TREE_SET_SIZE(n, TREE_GET_SIZE(n) - size);
 		n->addr += size;
 		return ret;
 	}
@@ -107,7 +111,7 @@ void* allocator_alloc_addr(uint64_t size, void* addr)
 	if(n == (void*)-1)
 		return (void*)-1;
 
-	if(n->size == size)
+	if(TREE_GET_SIZE(n) == size)
 	{ // perfect fit
 		alloc_tree_delete(n);
 		return addr;
@@ -116,19 +120,18 @@ void* allocator_alloc_addr(uint64_t size, void* addr)
 	{ // requested addr might be lying somewhere in [n->addr; n->addr + n->size - size]
 		// splitting the node into 2 parts
 		void *addr1 = n->addr, *addr2 = addr + size;
-		uint64_t size1 = (uint64_t)(addr - n->addr), size2 = (uint64_t)(n->addr + n->size - addr - size);
+		uint64_t size1 = (uint64_t)(addr - n->addr), size2 = (uint64_t)(n->addr + TREE_GET_SIZE(n) - addr - size);
 		alloc_tree_delete(n);
 		if(size1){
 			node* _new = alloc_node();
-			_new->addr = addr1; _new->size = size1;
+			_new->addr = addr1; TREE_SET_SIZE(_new, size1);
 			alloc_tree_insert(_new);
 		}
 		if(size2){
 			node* _new = alloc_node();
-			_new->addr = addr2; _new->size = size2;
+			_new->addr = addr2; TREE_SET_SIZE(_new, size2);
 			alloc_tree_insert(_new);
 		}
-
 		return addr;
 	}
 }
@@ -136,9 +139,9 @@ void* allocator_alloc_addr(uint64_t size, void* addr)
 node* merge_lr(node* n, void* addr)
 {
 	while(n){
-		if(n->addr + n->size == addr)
+		if(n->addr + TREE_GET_SIZE(n) == addr)
 			return n;
-		int dir = addr < n->addr + n->size ? TREE_DIR_LEFT : TREE_DIR_RIGHT;
+		int dir = addr < n->addr + TREE_GET_SIZE(n) ? TREE_DIR_LEFT : TREE_DIR_RIGHT;
 		n = n->child[dir];
 	}
 	return (void*)-1;
@@ -156,7 +159,7 @@ node* merge_rr(node* n, void* addr, uint64_t size)
 void allocator_free(void* addr, uint64_t size)
 {
 	node* _new = alloc_node();
-	_new->addr = addr; _new->size = size;
+	_new->addr = addr; TREE_SET_SIZE(_new, size);
 	alloc_tree_insert(_new);
 
 	node* merge_l = _new->child[TREE_DIR_LEFT] ? merge_lr(_new->child[TREE_DIR_LEFT], addr) : NULL;
@@ -164,11 +167,11 @@ void allocator_free(void* addr, uint64_t size)
 
 	if(merge_l != (void*)-1){
 		_new->addr = merge_l->addr;
-		_new->size += merge_l->size;
+		TREE_SET_SIZE(_new, TREE_GET_SIZE(_new) + TREE_GET_SIZE(merge_l));
 		alloc_tree_delete(merge_l);
 	}
 	if(merge_r != (void*)-1){
-		_new->size += merge_r->size;
+		TREE_SET_SIZE(_new, TREE_GET_SIZE(_new) + TREE_GET_SIZE(merge_r));
 		alloc_tree_delete(merge_r);
 	}
 }
@@ -180,7 +183,7 @@ void alloc_tree_insert(node* n)
 {
 	node* root = alloc_tree.root;
 	if(!root){
-		n->color = TREE_CLR_RED;
+		TREE_SET_CLR(n, TREE_CLR_RED);
 		n->child[TREE_DIR_LEFT] = n->child[TREE_DIR_RIGHT] = NULL;
 		n->parent = NULL;
 		alloc_tree.root = n;
@@ -204,7 +207,7 @@ void alloc_tree_insertp(node* n, node* p, int dir)
 	node	*g,		// grandparent
 		*u;		// uncle
 
-	n->color = TREE_CLR_RED;
+	TREE_SET_CLR(n, TREE_CLR_RED)
 	n->child[TREE_DIR_LEFT] = n->child[TREE_DIR_RIGHT] = NULL;
 	n->parent = p;
 	if(!p) // inserting at root
@@ -214,19 +217,19 @@ void alloc_tree_insertp(node* n, node* p, int dir)
 
 	do{
 		// case 1: parent is black, nothing to be done
-		if(p->color == TREE_CLR_BLACK)
+		if(TREE_GET_CLR(p) == TREE_CLR_BLACK)
 			return;
 		// parent is certainly red:
 
 		if(!(g = p->parent))
 		{ // case 4: parent is red and root
-			p->color = TREE_CLR_RED;
+			TREE_SET_CLR(p, TREE_CLR_RED);
 			return;
 		}
 
 		dir = TREE_DIR_CHILD(p); // at what side p is located in g
 		u = g->child[1 - dir];
-		if(!u || u->color == TREE_CLR_BLACK)
+		if(!u || TREE_GET_CLR(u) == TREE_CLR_BLACK)
 		{ // case 5-6: parent is red and uncle is black
 			if(n == p->child[1 - dir])
 			{ // case 5: parent is red, uncle is black, n is inner grandchild
@@ -236,15 +239,15 @@ void alloc_tree_insertp(node* n, node* p, int dir)
 			} // case 5 --> case 6
 			// case 6: parent is red, uncle is black, n is outer grandchild
 			alloc_tree_rotate(g, 1 - dir);
-			p->color = TREE_CLR_BLACK;
-			g->color = TREE_CLR_RED;
+			TREE_SET_CLR(p, TREE_CLR_BLACK);
+			TREE_SET_CLR(g, TREE_CLR_RED);
 			return;
 		}
 
 		// case 2: parent and uncle are red
-		p->color = TREE_CLR_BLACK;
-		u->color = TREE_CLR_BLACK;
-		g->color = TREE_CLR_RED;
+		TREE_SET_CLR(p, TREE_CLR_BLACK);
+		TREE_SET_CLR(u, TREE_CLR_BLACK);
+		TREE_SET_CLR(g, TREE_CLR_RED);
 		n = g;
 	} while((p = n->parent));
 }
@@ -272,27 +275,27 @@ void alloc_tree_delete_fixbb(node* n) // fix 2 black nodes in a row
 		alloc_tree_delete_fixbb(p);
 	}
 	else{
-		if(s->color == TREE_CLR_RED){
-			p->color = TREE_CLR_RED;
-			s->color = TREE_CLR_BLACK;
+		if(TREE_GET_CLR(s) == TREE_CLR_RED){
+			TREE_SET_CLR(p, TREE_CLR_RED);
+			TREE_SET_CLR(s, TREE_CLR_BLACK);
 			alloc_tree_rotate(p, TREE_DIR_CHILD(s));
 			alloc_tree_delete_fixbb(n);
 		}
 		else{
-			if((s->child[TREE_DIR_LEFT] && s->child[TREE_DIR_LEFT]->color == TREE_CLR_RED)
-			|| (s->child[TREE_DIR_RIGHT] && s->child[TREE_DIR_RIGHT]->color == TREE_CLR_RED))
+			if((s->child[TREE_DIR_LEFT] && TREE_GET_CLR(s->child[TREE_DIR_LEFT]) == TREE_CLR_RED)
+			|| (s->child[TREE_DIR_RIGHT] && TREE_GET_CLR(s->child[TREE_DIR_RIGHT]) == TREE_CLR_RED))
 			{ // at least 1 red child
-				if(s->child[TREE_DIR_LEFT] && s->child[TREE_DIR_LEFT]->color == TREE_CLR_RED)
+				if(s->child[TREE_DIR_LEFT] && TREE_GET_CLR(s->child[TREE_DIR_LEFT]) == TREE_CLR_RED)
 				{ // on the left
 					if(TREE_DIR_CHILD(s) == TREE_DIR_LEFT)
 					{ // left left
-						s->child[TREE_DIR_LEFT]->color = s->color;
-						s->color = p->color;
+						TREE_SET_CLR(s->child[TREE_DIR_LEFT], TREE_GET_CLR(s));
+						TREE_SET_CLR(s, TREE_GET_CLR(p));
 						alloc_tree_rotate(p, TREE_DIR_RIGHT);
 					}
 					else
 					{ // right left
-						s->child[TREE_DIR_LEFT]->color = p->color;
+						TREE_SET_CLR(s->child[TREE_DIR_LEFT], TREE_GET_CLR(p));
 						alloc_tree_rotate(s, TREE_DIR_RIGHT);
 						alloc_tree_rotate(p, TREE_DIR_LEFT);
 					}
@@ -301,26 +304,26 @@ void alloc_tree_delete_fixbb(node* n) // fix 2 black nodes in a row
 				{ // on the right
 					if(TREE_DIR_CHILD(s) == TREE_DIR_LEFT)
 					{ // left right
-						s->child[TREE_DIR_RIGHT]->color = p->color;
+						TREE_SET_CLR(s->child[TREE_DIR_RIGHT], TREE_GET_CLR(p));
 						alloc_tree_rotate(s, TREE_DIR_LEFT);
 						alloc_tree_rotate(p, TREE_DIR_RIGHT);
 					}
 					else
 					{ // right right
-						s->child[TREE_DIR_RIGHT]->color = s->color;
-						s->color = p->color;
+						TREE_SET_CLR(s->child[TREE_DIR_RIGHT], TREE_GET_CLR(s));
+						TREE_SET_CLR(s, TREE_GET_CLR(p));
 						alloc_tree_rotate(p, TREE_DIR_LEFT);
 					}
 				}
-				p->color = TREE_CLR_BLACK;
+				TREE_SET_CLR(p, TREE_CLR_BLACK);
 			}
 			else
 			{ // 2 black children
-				s->color = TREE_CLR_RED;
-				if(p->color == TREE_CLR_BLACK)
+				TREE_SET_CLR(s, TREE_CLR_RED);
+				if(TREE_GET_CLR(p) == TREE_CLR_BLACK)
 					alloc_tree_delete_fixbb(n);
 				else
-					p->color = TREE_CLR_BLACK;
+					TREE_SET_CLR(p, TREE_CLR_BLACK);
 			}
 		}
 	}
@@ -330,7 +333,7 @@ void alloc_tree_delete(node *n)
 	while(n)
 	{
 		node* u = alloc_tree_delete_replacement(n);
-		int un_is_black = (!u || u->color == TREE_CLR_BLACK) && n->color == TREE_CLR_BLACK;
+		int un_is_black = (!u || TREE_GET_CLR(u) == TREE_CLR_BLACK) && TREE_GET_CLR(n) == TREE_CLR_BLACK;
 
 		node* p = n->parent;
 
@@ -342,7 +345,7 @@ void alloc_tree_delete(node *n)
 					alloc_tree_delete_fixbb(n);
 				else
 					if(TREE_GET_SIBLING(n))
-						TREE_GET_SIBLING(n)->color = TREE_CLR_RED;
+						TREE_SET_CLR(TREE_GET_SIBLING(n), TREE_CLR_RED);
 
 				// delete n
 				p->child[TREE_DIR_CHILD(n)] = NULL;
@@ -354,7 +357,7 @@ void alloc_tree_delete(node *n)
 			// n has only 1 child
 			if(n == alloc_tree.root)
 			{ // replace n with it's child if n == root
-				n->addr = u->addr; n->size = u->size;
+				n->addr = u->addr; TREE_SET_SIZE(n, TREE_GET_SIZE(u));
 				n->child[TREE_DIR_LEFT] = n->child[TREE_DIR_RIGHT] = NULL;
 				kfree(u);
 			}
@@ -365,15 +368,15 @@ void alloc_tree_delete(node *n)
 				if(un_is_black)
 					alloc_tree_delete_fixbb(n);
 				else
-					u->color = TREE_CLR_BLACK;
+					TREE_SET_CLR(u, TREE_CLR_BLACK);
 				kfree(n);
 			}
 			return;
 		}
 		// swap u and n
 		node tmp;
-		tmp.size = n->size; tmp.addr = n->addr;
-		n->size = u->size; n->addr = u->addr;
+		tmp.size = TREE_GET_SIZE(n); tmp.addr = n->addr;
+		n->size = TREE_GET_SIZE(u); n->addr = u->addr;
 		u->size = tmp.size; u->addr = tmp.addr;
 		n = u;
 	}
@@ -382,7 +385,7 @@ void alloc_tree_delete(node *n)
 /* DFS by size */
 node* alloc_tree_find_first_fit_r(node* n, uint64_t size)
 {
-	if(n->size >= size)
+	if(TREE_GET_SIZE(n) >= size)
 		return n;
 
 	if(n->child[TREE_DIR_LEFT])
@@ -408,7 +411,7 @@ node* alloc_tree_find_containing(void* addr, uint64_t size)
 {
 	node* cur = alloc_tree.root;
 	while(cur){
-		if(addr >= cur->addr && addr + size <= cur->addr + cur->size)
+		if(addr >= cur->addr && addr + size <= cur->addr + TREE_GET_SIZE(cur))
 			return cur;
 		cur = (addr < cur->addr) ? cur->child[TREE_DIR_LEFT] : cur->child[TREE_DIR_RIGHT];
 	}
@@ -443,7 +446,7 @@ void alloc_tree_print_r(node* n, unsigned depth)
 		uart_printf("--\r\n"); // it's a leaf
 		return;
 	}
-	uart_printf("%c %p $ %p\r\n", n->color == TREE_CLR_BLACK ? 'B' : 'R', n->addr, (void*)n->size);
+	uart_printf("%c %p $ %p\r\n", TREE_GET_CLR(n) == TREE_CLR_BLACK ? 'B' : 'R', n->addr, (void*)TREE_GET_SIZE(n));
 
 	alloc_tree_print_r(n->child[TREE_DIR_LEFT], depth + 1);
 	alloc_tree_print_r(n->child[TREE_DIR_RIGHT], depth + 1);
